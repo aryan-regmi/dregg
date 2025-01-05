8adc08a1ed2fe339144ba1154e2dae1c90eab5b4
Aryan Regmi <aryan_regmi@protonmail.com>
2025-01-04 17:22:05 -0700

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
-use vizia::prelude::*;
+use iced::{
+    widget::{button, column},
+    Element, Task,
+};
 
-fn main() -> Result<(), ApplicationError> {
-    Application::new(|cx| {
-        let title = "    Dregg - DnD Character Creator";
-        Label::new(cx, title)
-            .width(Percentage(18.0))
-            // .width(Pixels((title.len() * 7) as f32))
-            .border_width(Pixels(2.0))
-            .border_color(Color::blue())
-            .background_color(Color::red());
-    })
-    .title("Dregg")
-    .inner_size((720, 480))
-    .run()
+fn main() -> iced::Result {
+    iced::run("Dregg", update, view)
+}
+
+#[derive(Default)]
+struct State {
+    screen: Screen,
+}
+
+#[derive(Default)]
+enum Screen {
+    #[default]
+    Main,
+    LoadCharacter,
+    NewCharacter,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Main,
+    LoadCharacter,
+    NewCharacter,
+}
+
+fn update(state: &mut State, message: Message) -> Task<Message> {
+    match message {
+        Message::Main => state.screen = Screen::Main,
+        Message::LoadCharacter => state.screen = Screen::LoadCharacter,
+        Message::NewCharacter => state.screen = Screen::NewCharacter,
+    }
+
+    Task::none()
+}
+
+fn view(state: &State) -> Element<Message> {
+    match state.screen {
+        Screen::Main => column![
+            button("Load Character").on_press(Message::LoadCharacter),
+            button("New Character").on_press(Message::NewCharacter),
+        ]
+        .into(),
+        Screen::LoadCharacter => button("Main Menu").on_press(Message::Main).into(),
+        Screen::NewCharacter => button("Main Menu").on_press(Message::Main).into(),
+    }
 }
