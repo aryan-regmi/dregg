5b07840babd1e284982dcad8722ca02b53e857b5
Aryan Regmi <aryan_regmi@protonmail.com>
2025-03-10 12:27:58 -0600

    Updated proficiencies

--- src/common/proficiency.rs
+++ src/common/proficiency.rs
@@ -21,19 +21,17 @@ pub enum ProficiencyLevel {
     Expertise,
 }
 
-// TODO: Update with actual inner types
-//
 /// Represents a type of proficiency.
 #[derive(Debug)]
 pub enum ProficiencyType {
     Armor(ArmorType),
-    Weapons,
-    Tools,
+    Weapons(WeaponType),
+    Tools(ToolType),
     SavingThrows(Attribute),
     Skills(Skills),
 }
 
-/// Represents an types of armors.
+/// Represents types of armors.
 #[derive(Debug)]
 pub enum ArmorType {
     /// Light armor.
@@ -47,4 +45,110 @@ pub enum ArmorType {
 
     /// Shields.
     Shield,
+}
+
+/// Represents types of weapons.
+#[derive(Debug)]
+pub enum WeaponType {
+    SimpleMelee,
+    SimpleRanged,
+    MartialMelee,
+    MaritalRanged,
+    Club,
+    Dagger,
+    Greatclub,
+    Handaxe,
+    Javelin,
+    LightHammer,
+    Mace,
+    Quarterstaff,
+    Sickle,
+    Spear,
+    LightCrossbow,
+    Dart,
+    Shortbow,
+    Sling,
+    Battleaxe,
+    Flail,
+    Glaive,
+    Greataxe,
+    Greatsword,
+    Halberd,
+    Lance,
+    Longsword,
+    Maul,
+    Morningstar,
+    Pike,
+    Rapier,
+    Scimitar,
+    Shortsword,
+    Trident,
+    WarPick,
+    Warhammer,
+    Whip,
+    Blowgun,
+    HandCrossbow,
+    HeavyCrossbow,
+    Longbow,
+    Net,
+}
+
+/// Represents types of tools.
+#[derive(Debug)]
+pub enum ToolType {
+    ArtisansTools(ArtisansTools),
+    DisguiseKit,
+    ForgeryKit,
+    GamingSet(GamingSet),
+    HerbalismKit,
+    MusicalInstrument(MusicalInstrument),
+    NavigatorsTools,
+    PoisonersKit,
+    ThievesTools,
+}
+
+/// Represents artisan tools.
+#[derive(Debug)]
+pub enum ArtisansTools {
+    AlchemistsSupplies,
+    BrewersSupplies,
+    CalligraphersSupplies,
+    CarpentersTools,
+    CartographersTools,
+    CobblersTools,
+    CooksUtensils,
+    GlassblowersTools,
+    JewelersTools,
+    LeatherworkersTools,
+    MasonsTools,
+    PaintersSupplies,
+    PottersTools,
+    SmithsTools,
+    TinkersTools,
+    WeaversTools,
+    WoodcarversTools,
+}
+
+/// Represents gaming sets.
+#[derive(Debug)]
+pub enum GamingSet {
+    DiceSet,
+    DragonchessSet,
+    PlayingCardSet,
+    ThreeDragonAnteSet,
+}
+
+/// Represents musical instruments.
+#[derive(Debug)]
+pub enum MusicalInstrument {
+    Bagpipes,
+    Drum,
+    Dulcimer,
+    Flute,
+    Lute,
+    Lyre,
+    Horn,
+    PanFlute,
+    Shawm,
+    Viol,
 }
