27a328082c8f65f50eb1ffa25f1c86f1e82e711e
Aryan Regmi <aryan_regmi@protonmail.com>
2025-03-10 14:59:15 -0600

    Started adding views

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,6 @@ pub mod class;
 pub mod common;
 pub mod equipment;
 pub mod race;
+pub mod views;
 
 // TODO: Add validation func @ the end (before finalizing new character)
---
+++ src/views/mod.rs
@@ -0,0 +1,14 @@
+use iced::Element;
+
+/// A type that can be displayed.
+pub trait Component {
+    type Message;
+    type Context;
+    type Command;
+
+    /// Describes how the component is drawn.
+    fn view(&self, ctx: Self::Context) -> Element<Self::Message>;
+
+    /// Describes how the component's state is updated.
+    fn update(&mut self, message: Self::Message) -> Self::Command;
+}
