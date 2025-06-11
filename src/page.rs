e45642f6116362b513b0edb653079547506ec218
Aryan Regmi <aryan_regmi@protonmail.com>
2025-06-11 15:25:25 -0600

    Removed unnecessary assignments

--- src/page.rs
+++ src/page.rs
@@ -54,27 +54,21 @@ impl Page {
                         Command::ChangePage(Pages::NewCharacter(new_character_page))
                     }
                     new_character_page::Command::RaceSelected(race) => {
-                        self.new_character_page_props.selected_race = Some(race.clone());
                         new_character_page.selected_race = Some(race.clone());
                         self.current = Pages::NewCharacter(new_character_page);
                         Command::UpdateSelectedRace(race)
                     }
                     new_character_page::Command::SubraceSelected(subrace) => {
-                        self.new_character_page_props.selected_subrace = Some(subrace.clone());
                         new_character_page.selected_subrace = Some(subrace.clone());
                         self.current = Pages::NewCharacter(new_character_page);
                         Command::UpdateSelectedSubrace(subrace)
                     }
                     new_character_page::Command::CustomRaceUpdated(race) => {
                         self.new_character_page_props.custom_race = race.clone();
-                        new_character_page.custom_race = race.clone();
                         self.current = Pages::NewCharacter(new_character_page);
                         Command::UpdateCustomRace(race)
                     }
                     new_character_page::Command::CustomRaceAdded(race) => {
-                        self.new_character_page_props
-                            .available_races
-                            .push(race.clone());
                         new_character_page.create_custom_race = false;
                         new_character_page.availabe_races.push(race.clone());
                         self.current = Pages::NewCharacter(new_character_page);
